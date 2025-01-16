7875f563d2bdad817d73b9d2e0c5108c84bc219e
Mavlon4658 <mavlonbe4658>
2025-01-16 09:02:20 +0500

    mobile is ready

--- js/main.js
+++ js/main.js
@@ -38,3 +38,22 @@ list[9].onmouseenter = () => {
 list[9].onmouseleave = () => {
     list[10].classList.remove('hidden');
 }
+
+const mbBox = document.querySelector('.mobile-box');
+const mbListWrap = document.querySelector('.mobile ul');
+const mbList = document.querySelectorAll('.mobile ul li');
+
+mbBox.onclick = () => {
+    mbListWrap.classList.add('active');
+}
+
+mbList.forEach(el => {
+    const btn = el.querySelector('button');
+    const h4 = el.querySelector('h4');
+    const p = el.querySelector('p');
+
+    btn.onclick = () => {
+        p.classList.add('active');
+        h4.classList.add('hidden');
+    }
+})
